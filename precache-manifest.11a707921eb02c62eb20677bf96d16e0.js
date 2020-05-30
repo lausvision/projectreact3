b58a4a237913887dfc3176ad4e1c3ac6be3ba858
lausvision <lauramolinalopez02@gmail.com>
2020-05-30 10:38:43 +0200

    Updates

---
+++ precache-manifest.11a707921eb02c62eb20677bf96d16e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9e021e0cc6d06290a830de2fe4b19f",
+    "url": "/projectreact3/index.html"
+  },
+  {
+    "revision": "6ba4025004fd2a111b60",
+    "url": "/projectreact3/static/css/main.cf2ce9dc.chunk.css"
+  },
+  {
+    "revision": "db7543d70a3f2c876089",
+    "url": "/projectreact3/static/js/2.1838a4f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projectreact3/static/js/2.1838a4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba4025004fd2a111b60",
+    "url": "/projectreact3/static/js/main.ae04055e.chunk.js"
+  },
+  {
+    "revision": "b0e218643b30279a4b81",
+    "url": "/projectreact3/static/js/runtime-main.61db9880.js"
+  },
+  {
+    "revision": "c8db90bd55a1513bbede7d6bc7a57592",
+    "url": "/projectreact3/static/media/hall.c8db90bd.jpg"
+  },
+  {
+    "revision": "c3de2c6f79a32e9a57fa39e163d7db9f",
+    "url": "/projectreact3/static/media/logo.c3de2c6f.png"
+  }
+]);
